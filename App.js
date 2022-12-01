2c6f28b1d205cb7e9d09e955e26be3d0ecf0aefc
Arpit Dhinwa <110625313+arpitdhinwa@users.noreply.github.com>
2022-12-01 13:16:59 +0530

    update

--- App.js
+++ App.js
@@ -1,35 +1,21 @@
-function home()
-{
-    document.querySelector("#img").src="./img.jpg";
-    document.querySelector("#home-link").classList.add("active");
-    document.querySelector("#work-link").classList.remove("active");
-    document.querySelector("#about-link").classList.remove("active");
-    document.querySelector("#blog-link").classList.remove("active");
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+
+listItems.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+
+        item.classList.add('active')
+        contents[idx].classList.add('show')
+    })
+})
+
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
 }
 
-function work()
-{
-    document.querySelector("#img").src="./img1.jpg";
-    document.querySelector("#home-link").classList.remove("active");
-    document.querySelector("#work-link").classList.add("active");
-    document.querySelector("#about-link").classList.remove("active");
-    document.querySelector("#blog-link").classList.remove("active");
-}
 
-function blog()
-{
-    document.querySelector("#img").src="./img2.jpg";
-    document.querySelector("#home-link").classList.remove("active");
-    document.querySelector("#work-link").classList.remove("active");
-    document.querySelector("#about-link").classList.remove("active");
-    document.querySelector("#blog-link").classList.add("active");
-}
-
-function about()
-{
-    document.querySelector("#img").src="./img3.jpg";
-    document.querySelector("#home-link").classList.remove("active");
-    document.querySelector("#work-link").classList.remove("active");
-    document.querySelector("#about-link").classList.add("active");
-    document.querySelector("#blog-link").classList.remove("active");
+function hideAllItems() {
+    listItems.forEach(item => item.classList.remove('active'))
 }
